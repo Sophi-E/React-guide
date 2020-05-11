a7871a66f9c25daeb47d5f8fdcec299ab6cb7280
Sophi-E <sophierithous@gmail.com>
2020-05-11 13:52:06 +0100

    added ingredients component and display burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import './Burger.css';
+import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
-const Burger = () => {
-  return <div></div>;
+const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    (ingredientKey) => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return (
+          <BurgerIngredients key={ingredientKey + i} type={ingredientKey} />
+        );
+      });
+    }
+  );
+  return (
+    <div className='Burger'>
+      <BurgerIngredients type='bread-top' />
+      {transformedIngredients}
+      <BurgerIngredients type='bread-bottom' />
+    </div>
+  );
 };
 
 export default Burger;
